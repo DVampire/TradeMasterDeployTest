aca42c8ee3b67f741ef7fbe23a96261c396a4e3b
DVampire <1271421361@qq.com>
2022-12-23 12:32:52 +0800

    add start date

--- backend_client_test.py
+++ backend_client_test.py
@@ -1,11 +1,9 @@
+import argparse
 import os
 import sys
-import requests
-import json
-import base64
+
 import pytz
-from glob import glob
-import argparse
+import requests
 
 tz = pytz.timezone('Asia/Shanghai')
 root = os.path.dirname(os.path.abspath(__file__))
@@ -14,29 +12,30 @@ sys.path.append(root)
 import random
 import string
 
+
 def generate_random_str(randomlength=16):
     str_list = [random.choice(string.digits + string.ascii_letters) for i in range(randomlength)]
     random_str = ''.join(str_list)
     return random_str
 
-def test_getParameters(request_url):
 
+def test_getParameters(request_url):
     headers = {
-        "Content-Type":"application/json"
+        "Content-Type": "application/json"
     }
     try:
-        response_return = requests.get(request_url,headers = headers).json()
+        response_return = requests.get(request_url, headers=headers).json()
         print(response_return)
     except Exception as e:
         print(e)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-ht','--host', type= str, default="127.0.0.1", help='request host')
-    parser.add_argument('-pt','--port', type= int , default=8080, help='request port')
-    parser.add_argument('-tn','--turn', type = int, default=1, help='request turn')
+    parser.add_argument('-ht', '--host', type=str, default="127.0.0.1", help='request host')
+    parser.add_argument('-pt', '--port', type=int, default=8080, help='request port')
+    parser.add_argument('-tn', '--turn', type=int, default=1, help='request turn')
 
     args = parser.parse_args()
 
